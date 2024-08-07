40d4d7e6f9b6d3560a0a0da8978d52cb880b768c
QuimeyC <quimeycerini@gmail.com>
2024-08-07 01:25:24 -0300

    Actulizacion

--- API_IOL2.vb
+++ API_IOL2.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Net
+Imports System.Reflection.Emit
 Imports System.Text
 Imports API_IOL2.API_IOL2
+Imports Newtonsoft
 
 
 Public Class API_IOL
@@ -68,6 +70,7 @@ Public Class API_IOL
         Return DateDiff(DateInterval.Second, Now, Me.Token.Time_Token) <= 0
     End Function
 
+
     Private Function VerificarToken() As Boolean
         If EstamosEnRangoTiempo() Then
             If Not RefrescarToken() Then
@@ -78,7 +81,7 @@ Public Class API_IOL
         End If
         Return True
     End Function
-    Public Function ObtenerEstadoDeCuentas() As EstadoDeCuentas
+    Public Function ObtenerEstadoDeCuentasOLD() As EstadoDeCuentas
 
         mensajeError = ""
         Try
@@ -112,6 +115,26 @@ Public Class API_IOL
             mensajeError = ex.Message
             Return Nothing
         End Try
+    End Function
+
+    Public Function ObtenerEstadoDeCuentas() As EstadoDeCuentas
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "api/v2/estadocuenta",
+            .Metodo = "GET"
+        }
+
+        Dim Respuesta As String = Solicitud.Send
+
+        Dim Resp As EstadoDeCuentas = JSonToObject(Of EstadoDeCuentas)(Respuesta)
+        If Resp Is Nothing Then
+            Return New EstadoDeCuentas() With {
+                .Json = Respuesta,
+                .DescripcionError = "Error: No se pudo obtener estdo de cuenta" & vbNewLine & mensajeError
+            }
+        Else
+            Return Resp
+        End If
+
     End Function
 
     Public Function RefrescarToken() As Boolean
@@ -271,83 +294,197 @@ Public Class API_IOL
         End Try
     End Function
 
-    Public Function Vender(Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional Plazo As String = "t2", Optional DiazValidez As Integer = 0, Optional Mercado As String = "BCBA") As String
+    Public Function ObtenerOpciones(Simbolo As String, Optional Mercado As String = "bcba") As Opciones
+
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "/api/v2/{mercado}/Titulos/{simbolo}/Opciones",
+            .Metodo = "GET"
+        }
+        Solicitud.Paths.Add("{simbolo}", Simbolo)
+        Solicitud.Paths.Add("{mercado}", Mercado)
+
+        Dim Respuesta As String = Solicitud.Send
+        Dim Resp As DatosOpciones = Newtonsoft.Json.JsonConvert.DeserializeObject(Of DatosOpciones)(Respuesta)
+        If Resp Is Nothing Then
+            Return New Opciones() With {
+            .DescripcionError = mensajeError,
+            .Json = Respuesta,
+            .ListaDeOpciones = New List(Of DatosOpcion)
+}
+        Else
+            Return New Opciones With {
+            .SimboloSubyacente = Simbolo,
+            .ListaDeOpciones = Resp
+            }
+        End If
+
+
+    End Function
+
+    Public Function ObtenerCotizacion(Simbolo As String, Optional plazo As String = "T2", Optional Mercado As String = "BCBA") As Cotizacion
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "api/v2/{Mercado}/Titulos/{Simbolo}/Cotizacion",
+            .Metodo = "GET"}
+        Solicitud.Paths.Add("{Mercado}", Mercado)
+        Solicitud.Paths.Add("{Simbolo}", Simbolo)
+
+        Solicitud.Parametros.Add("model.simbolo", Simbolo)
+        Solicitud.Parametros.Add("model.mercado", Mercado)
+        Solicitud.Parametros.Add("model.plazo", plazo)
+
+        Dim Respuesta As String = Solicitud.Send
+        Dim Resp As Cotizacion = JSonToObject(Of Cotizacion)(Respuesta)
+        If Resp Is Nothing Then
+            Return New Cotizacion() With {
+                .DescripcionError = mensajeError,
+                .Json = Respuesta
+            }
+        Else
+            Return Resp
+        End If
+
+
+    End Function
+
+    Public Function ObtenerInstrumentos(Optional pais As String = "argentina") As Pais
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "api/v2/{pais}/Titulos/Cotizacion/Instrumentos",
+            .Metodo = "GET"
+        }
+        Solicitud.Paths.Add("{pais}", pais)
+
+        Dim Respuesta As String = Solicitud.Send
+
+        Dim Resp As RespuestaPais = Newtonsoft.Json.JsonConvert.DeserializeObject(Of RespuestaPais)(Respuesta)
+        If Resp Is Nothing Then
+            Return New Pais() With {
+                .DescripcionError = mensajeError,
+                .Json = Respuesta,
+                .ListaInstrumentos = New RespuestaPais}
+        Else
+            Return New Pais() With {
+                .DescripcionError = "",
+                .Json = Respuesta,
+                .ListaInstrumentos = Resp}
+        End If
+    End Function
+    Public Function ObtenerNombrePaneles(Instrumento As String, Optional Pais As String = "argentina") As Paneles
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "api/v2/{pais}/Titulos/Cotizacion/Paneles/{instrumento}",
+            .Metodo = "GET"
+        }
+        Solicitud.Paths.Add("{pais}", Pais)
+        Solicitud.Paths.Add("{instrumento}", Instrumento)
+
+        Dim Respuesta As String = Solicitud.Send
+
+        Dim Resp As VariosPaneles = Newtonsoft.Json.JsonConvert.DeserializeObject(Of VariosPaneles)(Respuesta)
+        If Resp Is Nothing Then
+            Return New Paneles() With {
+                .DescripcionError = mensajeError,
+                .Json = Respuesta,
+                .ListaDePaneles = New VariosPaneles}
+        Else
+            Return New Paneles() With {
+                .DescripcionError = "",
+                .Json = Respuesta,
+                .ListaDePaneles = Resp}
+        End If
+    End Function
+
+    Public Function Vender(Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional Plazo As String = "t2", Optional DiazValidez As Integer = 0, Optional Mercado As String = "BCBA") As RespuestaOrden
         Return Operar("Vender", Simbolo, Cantidad, Precio, Plazo, DiazValidez, Mercado)
 
     End Function
 
-    Public Function Comprar(Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional Plazo As String = "t2", Optional DiazValidez As Integer = 0, Optional Mercado As String = "BCBA") As String
+    Public Function Comprar(Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional Plazo As String = "t2", Optional DiazValidez As Integer = 0, Optional Mercado As String = "BCBA") As RespuestaOrden
         Return Operar("Comprar", Simbolo, Cantidad, Precio, Plazo, DiazValidez, Mercado)
 
     End Function
 
+    Private Function Operar(Sentido As String, Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional plazo As String = "t2", Optional DiasValidez As Integer = 0, Optional Mercado As String = "BCBA") As RespuestaOrden
 
-    Private Function Operar(Sentido As String, Simbolo As String, Cantidad As Integer, Precio As Decimal, Optional Plazo As String = "t2", Optional DiazValidez As Integer = 0, Optional Mercado As String = "BCBA") As String
-        mensajeError = ""
-        Try
-            If Not VerificarToken() Then
-                mensajeError = "No se pudo obtener el token"
-                Return Nothing
-            End If
+        Dim Solicitud As New Solicitudes(Me) With {
+            .EndPoint = "api/v2/operar/" & Sentido,
+            .Metodo = "POST"
+        }
 
-            Dim FechaVto As DateTime = DateAdd(DateInterval.Day, DiazValidez, Now)
-            Dim sFechaVto As String = FechaVto.Year & "-" & FechaVto.Month & "-" & FechaVto.Day & "t17:59:59"
+        Dim FechaVto As DateTime = DateAdd(DateInterval.Day, DiasValidez, Now)
+        Dim sFechaVto As String = FechaVto.Year & "-" & FechaVto.Month & "-" & FechaVto.Day & "T17:59:59"
 
-            Dim sParametros = "Mercado" & Mercado
-            sParametros &= "&Validez" & sFechaVto
-            sParametros &= "&simbolo" & Simbolo
-            sParametros &= "&cantidad" & Cantidad
-            sParametros &= "&precio" & Precio
-            sParametros &= "&plazo" & Plazo
+        With Solicitud
+            .Parametros.Add("Mercado", Mercado)
+            .Parametros.Add("Validez", sFechaVto)
+            .Parametros.Add("simbolo", Simbolo)
+            .Parametros.Add("cantidad", Cantidad)
+            .Parametros.Add("precio", Precio.ToString("0.00").Replace(",", "."))
+            .Parametros.Add("plazo", plazo)
 
-            Dim Parametros = Encoding.UTF8.GetBytes(sParametros)
-            Dim respuesta As String = ""
-            Dim solicitud As WebRequest = WebRequest.Create(URL + "api/v2/operar/" & Sentido)
-            solicitud.Method = "POST"
-            solicitud.Headers.Add("Authorization", "Bearer " + Me.Token.Access_Token)
-            solicitud.PreAuthenticate = True
-            solicitud.ContentType = "application/x-www-form-urlencoded"
-            solicitud.ContentLength = Parametros.Length
+        End With
 
+        Dim Respuesta As String = Solicitud.Send
 
-            Using solicitudStream = solicitud.GetRequestStream
-                solicitudStream.Write(Parametros, 0, Parametros.Length)
-                solicitudStream.Close()
-            End Using
-
-            Using respuestaStream = solicitud.GetResponse.GetResponseStream
-                Using reader As New IO.StreamReader(respuestaStream)
-                    respuesta = reader.ReadToEnd()
-                End Using
-
-                Return respuesta
-
-
-            End Using
-
-
-
-        Catch ex As Exception
-            mensajeError = ex.Message
-            Return ""
-        End Try
+        Dim Resp As RespuestaOrden = JSonToObject(Of RespuestaOrden)(Respuesta)
+        If Resp Is Nothing Then
+            Return New RespuestaOrden With {
+                .Json = Respuesta,
+                .DescripcionError = mensajeError}
+        Else
+            Return Resp
+        End If
 
     End Function
 
 
 
     Public Function RefreshToken() As Boolean
-    mensajeError = ""
-    Return False
+        mensajeError = ""
+        Return False
 End Function
 
-End Class
 
-Public Class Token
+
+    Public Class Token
     Public Property Access_Token As String
     Public Property Refresh_Token As String
     Public Property Time_Token As DateTime
 
-
-
 End Class
+
+#Region "Funciones Auxiliares"
+
+Public Function JSonToObject(Of T)(JSon As String) As T
+    Try
+        If JSon = "" Then
+            Return Nothing
+        End If
+        Dim r As Object = Newtonsoft.Json.JsonConvert.DeserializeObject(Of T)(JSon)
+        CType(r, Respuesta).DescripcionError = ""
+        CType(r, Respuesta).Json = JSon
+        Return r
+    Catch ex As Exception
+        MensajeError = ex.Message
+        Return Nothing
+    End Try
+End Function
+
+#End Region
+Public Sub GuardarEnAchivo(archivo As String)
+    Try
+
+        IO.File.WriteAllText(archivo, Json.JsonConvert.SerializeObject(Me, Json.Formatting.Indented))
+    Catch ex As Exception
+
+    End Try
+End Sub
+Public Shared Function CargarDesdeArchivo(archivo As String) As Token
+    Try
+        Dim sjson As String = IO.File.ReadAllText(archivo)
+        Dim token As Token = Json.JsonConvert.DeserializeObject(Of Token)(sjson)
+        Return token
+    Catch ex As Exception
+        Return New Token
+    End Try
+End Function
+End Class
+
--- API_IOL2_Clases.vb
+++ API_IOL2_Clases.vb
@@ -1,6 +1,9 @@
-﻿Partial Public Class API_IOL2
+﻿Imports API_IOL2.API_IOL2
+
+Partial Public Class API_IOL2
 
     Public Class EstadoDeCuentas
+        Inherits Respuesta
         Public Property Cuentas As List(Of Cuenta)
     End Class
 
@@ -31,6 +34,7 @@
 
 
     Public Class Portafolio
+        Inherits Respuesta
         Public Property Pais As String
         Public Property Activos As List(Of Activo)
 
@@ -65,6 +69,7 @@
 
 
     Public Class Descripcion_Simbolo
+        Inherits Respuesta
         Public Property Simbolo As String
         Public Property Descripcion As String
         Public Property Pais As String
@@ -77,6 +82,7 @@
     End Class
 
     Public Class Panel
+        Inherits Respuesta
         Public Property Titulos As List(Of Titulo)
 
     End Class
@@ -109,6 +115,239 @@
         Public Property PrecioCompra As Decimal
         Public Property PrecioVenta As Decimal
         Public Property CantidadVenta As Decimal
+    End Class
+
+
+    Public Class RespuestaOrden
+        Inherits Respuesta
+        Public Property Ok As Boolean = True
+
+        Public Property Messages As List(Of Operacion_Message)
+
+        Public Property NumeroOperacion As Integer
+
+        Public Overloads ReadOnly Property TodoOK As Boolean
+            Get
+                Return Ok
+            End Get
+        End Property
+
+    End Class
+
+    Public Class Operacion_Message
+        Public Property Title As String
+        Public Property Description As String
+    End Class
+
+    Public Class Ordenes
+        Inherits Respuesta
+        Public Property Operaciones As RespuestaOperaciones
+
+    End Class
+
+
+    Public Class RespuestaOperaciones
+        Inherits List(Of Orden)
+    End Class
+
+    Public Class Orden
+        Inherits Respuesta
+        Public Property Numero As Integer?
+        Public Property FechaOrden As DateTime?
+        Public Property Tipo As String
+        Public Property Estado As String
+        Public Property Mercado As String
+        Public Property Simbolo As String
+        Public Property Cantidad As Decimal?
+        Public Property Monto As Decimal?
+        Public Property Modalidad As String
+        Public Property Precio As Decimal?
+        Public Property FechaOperada As DateTime?
+        Public Property CantidadOperada As Decimal?
+        Public Property PrecioOperado As Decimal?
+        Public Property MontoOperado As Decimal?
+
+    End Class
+
+    '----------------------------------------------
+    '/api/v2/{mercado}/Titulos/{simbolo}/Opciones
+    '----------------------------------------------
+    Public Class Opciones
+        Inherits Respuesta
+
+        Public Property ListaDeOpciones As DatosOpciones
+
+        Public Property SimboloSubyacente As String
+
+    End Class
+    Public Class DatosOpciones
+        Inherits List(Of DatosOpcion)
+
+    End Class
+
+    Public Class DatosOpcion
+
+        Public Property Cotizacion As Cotizacion
+
+        Public Property SimboloSubyacente As String
+        Public Property FechaVencimiento As Date
+        Public Property TipoOpcion As String
+        Public Property Simbolo As String
+        Public Property Descripcion As String
+        Public Property Pais As String
+        Public Property Mercado As String
+        Public Property Plazo As String
+        Public Property Moneda As String
+
+        Public ReadOnly Property Strike As Decimal
+            Get
+                Dim s() As String = Split(Descripcion, " ")
+                If s.Length > 2 Then
+                    Return Val(s(2))
+                Else
+                    Return 0
+                End If
+            End Get
+        End Property
+
+        Public Overrides Function ToString() As String
+            Dim s As String = Simbolo & " " & Descripcion & ",  $" & Cotizacion.UltimoPrecio.ToString("0.000")
+            Return s
+        End Function
+
+    End Class
+
+
+    ''' <summary>
+    ''' Mantiene los datos la cotización de un activo
+    ''' </summary>
+    Public Class Cotizacion
+        Inherits Respuesta
+
+        Public Property UltimoPrecio As Decimal
+        Public Property Variacion As Decimal
+        Public Property Apertura As Decimal
+        Public Property Maximo As Decimal
+        Public Property Minimo As Decimal
+
+        Public Property FechaHora As DateTime
+        Public Property Tendencia As String
+        Public Property CierreAnterior As Decimal
+        Public Property MontoOperado As Decimal
+        Public Property VolumenNominal As Decimal
+        Public Property PrecioPromeido As Decimal
+        Public Property Moneda As String
+
+        Public Property PrecioAjuste As Decimal
+        Public Property InteresesAbiertos As Decimal
+
+        Public Property Puntas As List(Of Punta)
+
+
+        Public Property CantidadOperaciones As Integer
+
+
+
+        Overrides Function ToString() As String
+            Dim s As String = "Precio: " & Me.UltimoPrecio & "   Variación:" & Me.Variacion & "  Operado:" & Me.MontoOperado & vbNewLine
+            s &= Me.Puntas.ToString()
+            Return s
+        End Function
+
+    End Class
+#Region "Clases para manejo de paneles e instrumentos"
+
+    Public Class Pais
+        Inherits Respuesta
+        Public Property ListaInstrumentos As RespuestaPais
+
+    End Class
+
+
+    Public Class RespuestaPais
+        Inherits List(Of Instrumento)
+
+    End Class
+    Public Class Instrumento
+        Public Property Instrumento As String
+        Public Property Pais As String
+    End Class
+
+
+    Public Class Paneles
+        Inherits Respuesta
+        Public Property ListaDePaneles As VariosPaneles
+    End Class
+
+    Public Class VariosPaneles
+        Inherits List(Of UnPanel)
+    End Class
+    Public Class UnPanel
+        Public Property Panel As String
+    End Class
+#End Region
+
+
+    '------------------------------------------------------------------------
+    ''' <summary>
+    ''' Mantiene los datos historicos de un simbolo
+    ''' </summary>
+    Public Class DatosHistoricos
+        Inherits Respuesta
+
+        Public Property Series As List(Of Cotizacion)
+
+
+
+        ''' <summary>
+        ''' Exportar a formato excel los datos de serie 
+        ''' </summary>
+        Public Function ExportarCSV(archivo As String, Optional Separador As String = ";") As Boolean
+            Dim Buffer As String = "Fecha" & Separador
+            Buffer &= "Ultimo" & Separador
+            Buffer &= "Àpertura" & Separador
+            Buffer &= "Máximo" & Separador
+            Buffer &= "Mínimo" & Separador
+            Buffer &= "Volumen" & Separador
+            Buffer &= "Monto operado" & vbNewLine
+
+            For Each c As Cotizacion In Series
+                Buffer &= c.FechaHora.ToString("yyyy-MM-dd") & Separador
+                Buffer &= c.UltimoPrecio.ToString("0.00") & Separador
+                Buffer &= c.Apertura.ToString("0.00") & Separador
+                Buffer &= c.Maximo.ToString("0.00") & Separador
+                Buffer &= c.Minimo.ToString("0.00") & Separador
+                Buffer &= c.VolumenNominal.ToString("0") & Separador
+                Buffer &= c.MontoOperado.ToString("0.00") & vbNewLine
+            Next
+            Try
+                IO.File.WriteAllText(archivo, Buffer)
+                Return True
+            Catch ex As Exception
+                Return False
+            End Try
+        End Function
+
+    End Class
+
+    ''' <summary>
+    ''' Clase Base utilizada para mantener el error y el json de todas las clase que puede devolver la libreria
+    ''' </summary>
+    Public Class Respuesta
+        Public Property DescripcionError As String = ""
+        Public Property Json As String = ""
+
+        Public ReadOnly Property TodoOk As Boolean
+            Get
+                Return DescripcionError.Length = 0
+            End Get
+        End Property
+        Public ReadOnly Property HuboError As Boolean
+            Get
+                Return Not TodoOk
+            End Get
+        End Property
+
     End Class
 
 End Class
